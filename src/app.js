7b36a34dc77c168bd0168c7c109745191bcfe3ba
mantanquzzmo <quzzmo@hotmail.com>
2019-11-21 10:03:30 +0100

    Configures remove button

--- src/app.js
+++ src/app.js
@@ -78,7 +78,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const clickedButton = event.target.id
         const contactNumber = clickedButton.replace('remove-btn-', '')
         const contacts = JSON.parse(window.localStorage.getItem('contacts'))
-        confirm(`Are you sure you want to remove ${contacts[contactNumber].name}?`);
+        removeContact = confirm(`Are you sure you want to remove ${contacts[contactNumber].name}?`)
+        if (removeContact == true) {
+          contacts.splice(contacts[contactNumber], 1)
+            const storage = window.localStorage
+            storage.setItem('contacts', JSON.stringify(contacts)) 
+            location.reload()
+        }
     })
 
 
